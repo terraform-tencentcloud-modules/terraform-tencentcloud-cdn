887d9f15fbc1e1dcc5290ea7c727c198ce6cdd1b
demoadminjie <demojie@outlook.com>
2023-03-09 18:45:51 +0800

    feat: complete the module examples

--- examples/simple-cdn-domain/outputs.tf
+++ examples/simple-cdn-domain/outputs.tf
@@ -1,19 +1,40 @@
-output "project_id" {
+output "ip_origin_project_id" {
   description = "Project CDN belongs to."
-  value       = module.cdn.project_id
+  value       = module.ip_cdn.project_id
 }
 
 output "cname" {
   description = "CNAME address of domain name."
-  value       = module.cdn.cname
+  value       = module.ip_cdn.cname
 }
 
 output "create_time" {
   description = "Domain name creation time."
-  value       = module.cdn.create_time
+  value       = module.ip_cdn.create_time
 }
 
 output "status" {
   description = "Acceleration service status."
-  value       = module.cdn.status
+  value       = module.ip_cdn.status
 }
+
+output "cos_origin_project_id" {
+  description = "Project CDN belongs to."
+  value       = module.cos_cdn.project_id
+}
+
+output "cname" {
+  description = "CNAME address of domain name."
+  value       = module.cos_cdn.cname
+}
+
+output "create_time" {
+  description = "Domain name creation time."
+  value       = module.cos_cdn.create_time
+}
+
+output "status" {
+  description = "Acceleration service status."
+  value       = module.cos_cdn.status
+}
+
---
+++ examples/simple-cdn-domain/variables.tf
@@ -0,0 +1,17 @@
+variable "domain" {
+  description = "Name of the acceleration domain."
+  type        = string
+  default     = ""
+}
+
+variable "ip" {
+  description = "Name of the acceleration ip."
+  type        = string
+  default     = ""
+}
+
+variable "cos_bucket" {
+  description = "Name of the acceleration cos_bucket."
+  type        = string
+  default     = ""
+}
